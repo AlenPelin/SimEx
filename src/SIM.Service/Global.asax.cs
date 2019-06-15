5b3bcff419b5d2e63d23bfd1f8939a995f43db91
Alen Pelin <alen.pelin@gmail.com>
2019-06-15 22:35:43 +1000

    Dummy service

---
+++ src/SIM.Service/Global.asax.cs
@@ -0,0 +1,14 @@
+﻿using SIM.Tool.Base;
+using System.Web;
+using System.Web.Http;
+
+namespace SIM.Service
+{
+  public class WebApiApplication : HttpApplication
+  {
+    protected void Application_Start()
+    {
+      GlobalConfiguration.Configure(WebApiConfig.Register);
+    }
+  }
+}
--- src/SIM.Service/Properties/AssemblyInfo.cs
+++ src/SIM.Service/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2a58b55a-f200-43be-95b1-d7c1b0e4e8a1")]
+[assembly: Guid("244f4787-dca8-42ea-abb5-9e30a247a041")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers
+// You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
